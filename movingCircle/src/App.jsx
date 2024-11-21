e90c7cac7fa3ab79b92e1a414a154732a20c397b
vaibhav <vaibhavning@gmail.com>
2024-11-21 17:18:32 +0530

    completed moving circle

--- movingCircle/src/App.jsx
+++ movingCircle/src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react"
 
 function App() {
- 
+ const [movement,setMovement]=useState({x:0,y:0});
+
+ const handleMouse=(event)=>{
+  setMovement({x:event.clientX,y:event.clientY})
+ }
 
   return (
     <>
-      <div className="bg-blue">Hello</div>
+       <div
+      className="w-screen h-screen bg-gray-100 "
+      onMouseMove={handleMouse}>
+      
+      <div
+        className="w-10 h-10 bg-blue-500 rounded-full fixed "
+        style={{
+          bottom: movement.y , // Center the circle
+          right: movement.x , // Center the circle
+        }}
+        ></div>
+      </div>
     </>
   )
 }
